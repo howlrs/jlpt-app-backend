f1372ec2f0d2dce9c61f88d6a788612be47cb8f8
howlrs <sharebook.amazon@gmail.com>
2025-03-19 12:57:04 +0900

    add data row normalize

--- src/api/question.rs
+++ src/api/question.rs
@@ -1,4 +1,4 @@
-use std::sync::Arc;
+use std::{fs, sync::Arc};
 
 use axum::{
     extract::{Path, Query, State},
@@ -123,6 +123,8 @@ pub async fn get(
         None => questions,
     };
 
+    println!("limit: {}", questions.len());
+
     info!("limit: {}", questions.len());
 
     response_handler(
@@ -168,6 +170,113 @@ async fn read_db(
         Err(e) => {
             eprintln!("Error: {:?}", e);
             vec![]
+        }
+    }
+}
+
+async fn normalize_document_types(
+    i: i32,
+    db: Arc<crate::common::database::Database>,
+) -> Result<(), Box<dyn std::error::Error>> {
+    println!("データ型の正規化を開始します...");
+
+    let current_dir = std::env::current_dir().unwrap();
+    let keys = fs::read_to_string(current_dir.join("success_ids.json"))?;
+    let keys: Vec<String> = serde_json::from_str(&keys)?;
+    // set hashmap
+    let hashmap: std::collections::HashMap<String, ()> =
+        keys.into_iter().map(|key| (key, ())).collect();
+
+    // success id length
+    println!("successed id length: {}", hashmap.len());
+
+    // レベルIDが1,4,5のドキュメントだけを取得
+    let mut questions_stream = db
+        .client
+        .fluent()
+        .select()
+        .from("questions")
+        .filter(|q| {
+            q.for_all([
+                q.for_any([
+                    q.field(path!(Question::level_id)).eq(1),
+                    q.field(path!(Question::level_id)).eq(4),
+                    q.field(path!(Question::level_id)).eq(5),
+                ]),
+                q.field(path!(Question::category_id)).eq(format!("{}", i)),
+            ])
+        })
+        .obj::<Question>()
+        .stream_query_with_errors()
+        .await?;
+
+    let mut total_processed = 0;
+    let mut success_ids = vec![];
+
+    while let Some(result) = questions_stream.next().await {
+        match result {
+            Ok(question) => {
+                // hashmapに存在する場合はスキップ
+                if hashmap.contains_key(&question.id) {
+                    println!("スキップ: {}", question.id);
+                    continue;
+                }
+
+                match db
+                    .update::<Question>("questions", &question.id, question.clone())
+                    .await
+                {
+                    Ok(_) => {
+                        total_processed += 1;
+                        println!("ドキュメント更新: {} - {}", total_processed, question.id);
+                        success_ids.push(question.id);
+
+                        // tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+                    }
+                    Err(e) => {
+                        eprintln!("ドキュメント更新エラー: {:?}", e);
+                        tokio::time::sleep(tokio::time::Duration::from_secs(300)).await;
+                    }
+                }
+            }
+            Err(e) => {
+                eprintln!("ドキュメント読み込みエラー: {:?}", e);
+                tokio::time::sleep(tokio::time::Duration::from_secs(300)).await;
+            }
+        }
+    }
+
+    println!(
+        "データ型の正規化が完了しました。合計{}件のドキュメントを処理しました。",
+        total_processed
+    );
+
+    // save file to success_ids
+    let success_ids = serde_json::to_string(&success_ids)?;
+    std::fs::write("success_ids.json", success_ids)?;
+
+    Ok(())
+}
+
+#[cfg(test)]
+// normalize_document_typesのテスト稼働
+mod tests {
+    use super::*;
+    use crate::common::database::Database;
+    use std::sync::Arc;
+
+    #[tokio::test]
+    async fn test_normalize_document_types() {
+        // load .env.local
+        let current_dir = std::env::current_dir().unwrap();
+        let target_env = current_dir.join(".env.local");
+        dotenv::from_filename(target_env).ok();
+
+        let db = Arc::new(Database::new().await);
+        let max = 24;
+        for i in 3..max {
+            println!("category_id: {}の処理を開始します...", i);
+            normalize_document_types(i, db.clone()).await.unwrap();
         }
     }
 }
